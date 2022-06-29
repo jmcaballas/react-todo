6790c1ae323b5bb15b5381a94aec5fabbbf47e89
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-29 22:04:50 +0800

    Allow creation of newTodoItems

--- src/App.js
+++ src/App.js
@@ -20,9 +20,14 @@ function App() {
     },
   ]);
 
+  const createTodoItem = (todo) => {
+    const newTodoItems = [...todoItems, { todo, complete: false }];
+    setTodoItems(newTodoItems);
+  };
+
   return (
     <div className="">
-      <TodoInput />
+      <TodoInput createTodoItem={createTodoItem} />
       {todoItems.map((item, index) => (
         <TodoItem key={index} index={index} item={item} />
       ))}
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,8 +1,27 @@
-const TodoInput = () => {
+import { useState } from "react";
+
+const TodoInput = ({ createTodoItem }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value === "") {
+      return alert("Please add something to do");
+    }
+    createTodoItem(value);
+    setValue("");
+  };
+
   return (
-    <div>
-      <h1>TodoInput</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add something to do"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+    </form>
   );
 };
 
