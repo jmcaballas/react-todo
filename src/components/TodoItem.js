2e730e87bac06e2d5e1d9500d64655d9a3f15f07
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-29 22:41:48 +0800

    Add styling to TodoItems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,16 +6,30 @@ const TodoItem = ({
   editTodoItem,
 }) => {
   return (
-    <div>
-      <li>
-        <input type="checkbox" onChange={() => doneTodoItem(index)} />
+    <div className="container card text-bg-light d-flex flex-row align-items-center justify-content-between">
+      <li className="d-flex gap-3 align-items-center">
+        <input
+          type="checkbox"
+          onChange={() => doneTodoItem(index)}
+          className="form-check-input"
+        />
         <h3 style={{ textDecoration: item.complete ? "line-through" : "" }}>
           {item.todo}
         </h3>
       </li>
-      <div>
-        <button onClick={() => editTodoItem(index)}>Edit</button>
-        <button onClick={() => deleteTodoItem(index)}>Delete</button>
+      <div className="d-flex gap-3">
+        <button
+          onClick={() => editTodoItem(index)}
+          className="btn btn-warning shadow-none"
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => deleteTodoItem(index)}
+          className="btn btn-danger shadow-none"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
