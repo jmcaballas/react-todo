1a821c58c91bc574dc6576dfb4d3944f74635bda
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-29 21:50:46 +0800

    Import TodoInput and TodoItem in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <TodoInput />
+      <TodoItem />
+    </div>
+  );
 }
 
 export default App;
